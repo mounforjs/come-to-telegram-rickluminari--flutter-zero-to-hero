7cfdd4a889035fd946f93cf8be0685454a5a2824
mounforjs <mounford.js@gmail.com>
2026-04-17 19:56:46 -0400

    Adding state with currentIndex variable, unsing conditional

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const MyApp());
 }
 
+//stateful - can refresh the screen
+//stateless - cannot refresh the screen
+//setState - refresh the screen
+
 String title = "My Title";
 
 class MyApp extends StatelessWidget {
@@ -20,24 +24,58 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title!, style: TextStyle(color: Colors.white)),
-        ), //ssd
-        bottomNavigationBar: NavigationBar(
-          selectedIndex: 0,
-          onDestinationSelected: (int value) {
-            print(value);
-          },
-
-          //Your required at least 2 destinations in order to show the navigation bar
-          //Otherwise, it will not show the navigation bar and you will get an error
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'None'), //sd
-            NavigationDestination(icon: Icon(Icons.person), label: 'None'), //sd
-          ],
-        ),
-      ),
+      home: myHomePage(),
     ); //sd
+  }
+}
+
+class myHomePage extends StatefulWidget {
+  const myHomePage({super.key});
+
+  @override
+  State<myHomePage> createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!, style: TextStyle(color: Colors.white)),
+      ), //ssd
+      body: currentIndex == 0
+          ? Center(
+              child: Text(
+                "Home Page",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            )
+          : Center(
+              child: Text(
+                "Profile Page",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: (int value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+
+        //Your required at least 2 destinations in order to show the navigation bar
+        //Otherwise, it will not show the navigation bar and you will get an error
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'None'), //sd
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ), //sd
+        ],
+      ),
+    );
   }
 }
